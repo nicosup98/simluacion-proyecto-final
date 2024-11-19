ff98ea620909b25a46e37f33692ebb51bd7b6d5f
nicosup <damiconicola98@gmail.com>
2024-11-19 15:57:43 -0400

    first commit

---
+++ info.md
@@ -0,0 +1,79 @@
+Se desea aplicar un proceso de simulación de sistemas a los efectos de resolver un problema que aqueja a la colectividad de usuarios de la vía rápida (circunvalación  1)  de la ciudad de Maracaibo, Estado Zulia. En relación a la problemática existente actualmente se requiere instrumentar medidas que solucionen la saturación de vehículos automotores y transito general por las limitaciones espaciales del trazado vial en cuestión.
+
+El ente gubernamental encargado de la materia vial en el país el Ministerio del Poder Popular para Transporte Terrestre y Obras Publicas, diseño una solución (Figura # 1) que resolvería la problemática existente parcialmente (para los próximos 10 años) sin considerar obviamente el tiempo de ejecución de la obra.
+
+La propuesta comprende la construcción de una vía aérea tipo superficie auto soportada que conste de tres (3) canales de circulación, dos (2) para el trafico exclusivo de vehículos ligeros y uno (1) reservado exclusivamente para situaciones de emergencia, la circulación por restricciones de seguridad internacionales solo se podrá efectuar en un solo sentido, pero en la propuesta se contempla el redireccionamiento para ambos sentidos en las horas que se consideren convenientes.
+
+A continuación se especifican una serie de estadísticas que maneja el ente ejecutor del proyecto:
+
+
+Densidad Vehicular horas pico:
+
+Sentido Norte – Sur   (LUNES A VIERNES)
+6:00 a.m. – 9:00 a.m.    119 vehiculos
+11:30 a.m. – 1:00 p.m.  105 vehiculos
+5:00 p.m. – 7:30 p.m.   120 vehiculos 
+
+
+
+
+
+Sentido Sur – Norte    (LUNES A VIERNES)
+6:00 a.m. – 9:00 a.m.    117 vehiculos	
+11:30 a.m. – 1:00 p.m.  98  vehiculos
+5:00 p.m. – 9:15 p.m.    76  vehiculos
+
+Sentido Norte – Sur   (SABADO A DOMINGO)
+1:00 p.m. – 3:00 p.m.    107 vehiculos
+6:00 a.m. – 8:00 p.m.    80 vehiculos
+
+Sentido Sur – Norte    (SABADO A DOMINGO)
+7:00 a.m. – 9:30 a.m.    105 vehiculos
+4:30 a.m. – 10:00 p.m.  54 vehiculos
+
+Tope de Flujo Vehicular   125 vehículos / kilómetro
+
+Operatividad Plena de la Vía Actualmente
+
+4 Canales de circulación por sentido:
+
+3 Trafico Normal.
+1 (Solo Servicio) de Emergencia.
+
+Interrupciones del Transito en la vía para labores de:
+	
+Mantenimiento Áreas Verdes.
+	Mantenimiento Sistemas Eléctricos.
+	Reparaciones menores en vía.
+	Colisiones Varias.
+	Cierres Preventivos.
+	Manifestaciones Generales (Colectividad y sectores Particulares).
+
+
+Sentido Norte – Sur   1 Canal (12 Kilómetros)  350 minutos / año
+
+Sentido Sur – Norte   1 Canal (12 Kilómetros)  197 minutos / año
+
+
+Tiempo promedio en cola durante un recorrido(completo) por sentido:
+
+Norte – Sur  (LUNES A VIERNES)   18 minutos por viaje
+
+Sur – Norte  (LUNES A VIERNES)     6 minutos por viaje
+
+Norte – Sur  (SABADO A DOMINGO)   8 minutos por viaje
+
+Sur – Norte  (SABADO A DOMINGO)     Sin Demora
+
+
+
+Para el caso de días Festivos Nacionales, Regionales, 24 y 31 de Diciembre se alcanza el Tope de Flujo Vehicular.
+
+Según los detalles y especificaciones suministrados, usted deberá establecer un proceso de Simulación de Sistema que especifique la manera optima de organizar el sentido vehicular de la nueva vía proyectada  además de aportar la sincronización del sentido de acuerdo al comportamiento del tráfico en horas picos si lo considera relevante.
+
+La interfaz de usuario contendrá la siguiente estructura:
+
+Fecha y hora de inicio de simulación:    dd/mm/aa    hh/mm
+Fecha y hora de fin de simulación:        dd/mm/aa    hh/mm
+
+Existe una restricción para el cambio de sentido de circulación en la nueva vía    120 minutos para la descarga de vehículos en el trazado de no existir ningún vehiculo en circulación o accidentado.
---
+++ main.py
@@ -0,0 +1,113 @@
+import random
+import time
+from datetime import datetime, timedelta,time as Dtime
+
+# Configuración inicial
+DATA = {
+    "Norte-Sur": {
+        "Lunes-Viernes": {"vehiculos": [["06:00", "09:00", 119], ["11:30", "13:00", 105], ["13:00", "19:30", 120]], "demora": 18},
+        "Sabado-Domingo": {"vehiculos": [["13:00", "15:00", 107], ["06:00", "20:00", 80]], "demora": 8},
+        "mantenimiento": 350
+    },
+    "Sur-Norte": {
+        "Lunes-Viernes": {"vehiculos": [["06:00", "09:00", 117], ["11:30", "13:00", 98], ["17:00", "21:15", 76]], "demora": 6},
+        "Sabado-Domingo": {"vehiculos": [["07:00", "09:00", 105], ["04:30", "22:00", 54]], "demora": 0},
+        "mantenimiento": 197
+    }
+}
+
+TOPE_FLUJO_VEHICULAR = 125  # vehículos/km
+TOPE_FLUJO_VEHICULAR_REPARACION = round(TOPE_FLUJO_VEHICULAR * 0.77)
+CANAL_EMERGENCIA = 1  # Canal reservado para emergencias
+CANAL_NORMAL = 3  # Canales para tráfico normal
+
+
+def main():
+    sim = {"Norte-Sur":{}, "Sur-Norte":{}}
+    random.seed(time.time())
+    inicio = input("fecha inicio (dd/mm/aa hh:mm): ")
+    fin = input("fecha fin (dd/mm/aa hh:mm): ")
+    fecha_inicio = datetime.strptime(inicio,"%d/%m/%y %H:%M")
+    fecha_fin = datetime.strptime(fin, "%d/%m/%y %H:%M")
+    
+    [sim["Norte-Sur"]["demora"],sim["Norte-Sur"]["autos"]] = simulacion("Norte-Sur",fecha_inicio,fecha_fin)
+    [sim["Sur-Norte"]["demora"],sim["Sur-Norte"]["autos"]] = simulacion("Sur-Norte",fecha_inicio,fecha_fin)
+    for via in ["Norte-Sur","Sur-Norte"] :
+        [sim[via]["demora"],sim[via]["autos"]] = simulacion(via,fecha_inicio,fecha_fin)
+    
+    print(f"en el intervalo de {fecha_inicio} a {fecha_fin}")
+    
+    for k,v in sim.items():
+        print(f"autos que recorrieron la via {k}: {v["autos"]}, demora de los autos en la via: {v["demora"]}")
+        
+def simulacion(sentido,fecha_inicio,fecha_fin):
+    delta = fecha_inicio
+    tiempo_consumido = 0
+    cantidad_autos = 0
+    while delta < fecha_fin:
+        [tiempo_trafico,autos]= sim_trafico(delta,sentido)
+        delta += timedelta(minutes=tiempo_trafico + 1)
+        # print(delta)
+        tiempo_consumido += tiempo_trafico
+        cantidad_autos += autos
+    return [tiempo_consumido,cantidad_autos]
+    
+    
+def autos_via(t_actual: datetime, vehiculos: list, flujo_vehicular):
+    cantidad_vehiculos = 0
+    for h in vehiculos:
+        [inicio,fin,cantidad] = h
+        hora_inicio = Dtime.fromisoformat(inicio)
+        hora_fin = Dtime.fromisoformat(fin)
+        
+        if t_actual.time() > hora_inicio and t_actual.time() <= hora_fin:
+            promedio_autos_intervalo = cantidad / (((hora_fin.hour - hora_inicio.hour) * 60) + hora_fin.minute)
+            cantidad_vehiculos += random.randint(round(promedio_autos_intervalo * 0.9),round(promedio_autos_intervalo * 1.1))
+            break
+        else:
+            cantidad_vehiculos = round(TOPE_FLUJO_VEHICULAR * 0.1)
+        
+    if cantidad_vehiculos > flujo_vehicular:
+        cantidad_vehiculos = flujo_vehicular
+        
+    return cantidad_vehiculos
+
+def sim_trafico(t_actual: datetime,sentido):
+    dia = "Lunes-Viernes" if t_actual.weekday() < 5 else "Sabado-Domingo"
+    info = DATA[sentido][dia]
+    demora = info["demora"]
+    flujo_vehicular = TOPE_FLUJO_VEHICULAR
+    if random.random() < 0.01:
+        [intr,duracion_intr]= interrupcion(sentido)
+        print(f"ocurrio una interrupcion en el sentido {sentido}, causa: {intr}, demora: {duracion_intr}")
+        demora += duracion_intr
+        flujo_vehicular = TOPE_FLUJO_VEHICULAR_REPARACION
+        
+    autos = autos_via(t_actual,info["vehiculos"],flujo_vehicular)
+    
+    if autos == flujo_vehicular:
+        demora += 1
+    
+    
+    return [random.randint(round(demora* 0.9),round(demora* 1.1)),autos]
+    
+    
+
+def interrupcion(sentido):
+    interrupciones = [	"Mantenimiento Sistemas Eléctricos",
+                        "Reparaciones menores en vía.",
+	                    "Colisiones Varias.",
+	                    "Cierres Preventivos.",
+	                    "Manifestaciones Generales (Colectividad y sectores Particulares)"
+                     ]
+    
+    int_actual = random.choice(interrupciones)
+    duracion_max_intr = DATA[sentido]["mantenimiento"] 
+    duracion_intr = random.randint(5,duracion_max_intr) if duracion_max_intr > 0 else 0
+    DATA[sentido]["mantenimiento"] -= duracion_intr
+    return [int_actual,duracion_intr]
+    
+    
+if __name__=="__main__":
+    main()
+
